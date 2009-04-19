17b7090b59264c366d7a11be07ee6e61bde451a4
Simon Josefsson <simon@josefsson.org>
2009-04-19 05:39:30 +0000

    Add.

---
+++ ykksm-export.pl
@@ -0,0 +1,111 @@
+#!/usr/bin/perl
+
+# Written by Simon Josefsson <simon@josefsson.org>.
+# Copyright (c) 2009 Yubico AB
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+#   * Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+#
+#   * Redistributions in binary form must reproduce the above
+#     copyright notice, this list of conditions and the following
+#     disclaimer in the documentation and/or other materials provided
+#     with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+use strict;
+use DBI;
+use MIME::Base64;
+
+sub usage {
+    print "Usage: $0 [--verbose] [--help]\n";
+    print "          [--database DBI] [--db-user USER] [--db-passwd PASSWD]\n";
+    print "\n";
+    print "  Tool to export keys to the YKKSM-KEYPROV format.\n";
+    print "\n";
+    print "  --database DBI: Database identifier, see http://dbi.perl.org/\n";
+    print "                  defaults to a MySQL database ykksm on localhost,\n";
+    print "                  i.e., DBI::mysql:ykksm.\n";
+    print "\n";
+    print "  --db-user USER: Database username to use, defaults to empty string.\n";
+    print "\n";
+    print "  --db-passwd PASSWD: Database password to use, defaults to empty string.\n";
+    print "\n";
+    print "Usage example:\n";
+    print "\n";
+    print "  ./ykksm-export.pl --db-user foo --db-passwd pass123 |\n";
+    print "     gpg -a --encrypt -r 1D2F473E > keys.txt\n";
+    print "\n";
+    exit 1;
+}
+
+my $verbose = 0;
+my $db = "dbi:mysql:ykksm";
+my $dbuser;
+my $dbpasswd;
+while ($ARGV[0] =~ m/^-(.*)/) {
+    my $cmd = shift @ARGV;
+    if (($cmd eq "-v") || ($cmd eq "--verbose")) {
+	$verbose = 1;
+    } elsif (($cmd eq "-h") || ($cmd eq "--help")) {
+	usage();
+    } elsif ($cmd eq "--database") {
+	$db = shift;
+    } elsif ($cmd eq "--db-user") {
+	$dbuser = shift;
+    } elsif ($cmd eq "--db-passwd") {
+	$dbpasswd = shift;
+    }
+}
+
+if ($#ARGV>=0) {
+    usage();
+}
+
+my $dbh = DBI->connect($db, $dbuser, $dbpasswd, {'RaiseError' => 1});
+my $sth = $dbh->prepare
+    ('SELECT serialNr, publicName, internalName, aesKey, lockCode, creator FROM yubikeys')
+    or die "Couldn't prepare statement: " . $dbh->errstr;
+$sth->execute()
+    or die "Couldn't execute statement: " . $sth->errstr;
+
+my $row;
+while ($row = $sth->fetchrow_hashref()) {
+    if ($verbose) {
+	print "# ";
+	foreach my $key (keys %$row) {
+	    print $key . "=" . $row->{$key} . " ";
+	}
+	print "\n";
+    }
+    print $row->{'serialNr'} . ",";
+    print $row->{'publicName'} . ",";
+    print $row->{'internalName'} . ",";
+    print $row->{'aesKey'} . ",";
+    print $row->{'lockCode'} . ",";
+    print $row->{'creator'} . ",\n";
+}
+
+if ($sth->rows == 0) {
+    print "No data?!\n\n";
+}
+
+$sth->finish;
+$dbh->disconnect();
+
+exit 0;
