24e030ff8559c132af2be1eddd54facd24111e5c
Simon Josefsson <simon@josefsson.org>
2009-12-14 14:08:02 +0000

    Make portable.

--- ykksm-db.sql
+++ ykksm-db.sql
@@ -1,16 +1,10 @@
--- drop database ykksm;
-create database ykksm;
-use ykksm;
-
 create table yubikeys (
-  id int not null auto_increment,
-
   -- identities:
   serialNr int not null,
   publicName varchar(16) unique not null,
 
   -- timestamps:
-  created datetime not null,
+  created varchar(24) not null,
 
   -- the data:
   internalName varchar(12) not null,
@@ -24,14 +18,5 @@ create table yubikeys (
   active boolean default true,
   hardware boolean default true,
 
-  primary key (id)
-  -- MySQL generates an index automatically for columns marked 'unique'
-  -- if this doesn't seem to happen, add: 'key (publicName)'
-  -- use, e.g., explain select * from yubikeys where publicName = 'foo';
-  -- and make sure it uses a key to find the column.
+  primary key (publicName)
 );
-
--- drop user ykksmreader;
-create user ykksmreader;
-grant select on ykksm.yubikeys to 'ykksmreader'@'localhost';
-flush privileges;
