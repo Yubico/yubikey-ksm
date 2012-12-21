6279082493b8a150dab0af22385b0b3c4d2b6138
Dain Nilsson <dain@yubico.com>
2012-12-21 11:17:55 +0100

    Added support for reading db config from /etc/ykksm/

--- ykksm-config.php
+++ ykksm-config.php
@@ -1,7 +1,18 @@
 <?php
-$db_dsn      = "mysql:dbname=ykksm;host=127.0.0.1";
-$db_username = "ykksmreader";
-$db_password = "yourpassword";
+//ykksm will use the configuration stored in /etc/ykksm/config-db.php, if that file exists. If it does not exist, the below values will be used.
+
+if(!include '/etc/ykksm/config-db.php') {
+	$dbuser='ykksmreader';
+	$dbpass='yourpassword';
+	$basepath='';
+	$dbname='ykksm';
+	$dbserver='';
+	$dbport='';
+}
+
+$db_dsn      = "$dbtype:dbname=$dbname;host=127.0.0.1";
+$db_username = $dbuser;
+$db_password = $dbpass;
 $db_options  = array();
-$logfacility = LOG_LOCAL0;
+$logfacility = LOG_AUTH;
 ?>
