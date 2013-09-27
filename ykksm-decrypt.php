fff4394ca77cd0d48cd416306b01f9217de11be4
Mrten <bugzilla@ii.nl>
2013-09-27 11:34:51 +0200

    First test for parameter, then use it
    One should test if a parameter exists before using it instead of letting the PHP auto-type conversion magic have its way with it.

--- ykksm-decrypt.php
+++ ykksm-decrypt.php
@@ -34,11 +34,11 @@ require_once 'ykksm-utils.php';
 openlog("ykksm", LOG_PID, $logfacility)
   or die("ERR Syslog open error\n");
 
-$otp = $_REQUEST["otp"];
-if (!$otp) {
+if (!isset($_REQUEST["otp"])) {
   syslog(LOG_INFO, "No OTP provided");
-  die("ERR No OTP provided\n");
+  die("ERR Invalid OTP format\n");
  }
+$otp = $_REQUEST["otp"];
 
 if (!preg_match("/^([cbdefghijklnrtuv]{0,16})([cbdefghijklnrtuv]{32})$/",
 		$otp, $matches)) {
