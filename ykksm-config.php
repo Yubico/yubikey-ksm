829d896c2156ff96f53c645af22cf0907559cab6
Cam34 <cam34@github.com>
2015-05-28 03:52:15 +0000

    Modify config-db.php include to check for file instead of doing a (failed) include and spamming the log files that the file doesn't exist

--- ykksm-config.php
+++ ykksm-config.php
@@ -1,7 +1,9 @@
 <?php
 //ykksm will use the configuration stored in /etc/yubico/ksm/config-db.php, if that file exists. If it does not exist, the below values will be used.
 
-if(!include '/etc/yubico/ksm/config-db.php') {
+if(file_exists('/etc/yubico/ksm/config-db.php')) {
+	include '/etc/yubico/ksm/config-db.php';
+} else {
 	$dbuser='ykksmreader';
 	$dbpass='yourpassword';
 	$basepath='';
@@ -10,7 +12,6 @@ if(!include '/etc/yubico/ksm/config-db.php') {
 	$dbport='';
 	$dbtype='mysql';
 }
-
 $db_dsn      = "$dbtype:dbname=$dbname;host=127.0.0.1";
 $db_username = $dbuser;
 $db_password = $dbpass;
